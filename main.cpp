36b8d637e6db645d6b47cf1ef76bd6d87661e9a4
guilhemheinrich <guilhem.heinrich@gmail.com>
2016-01-25 10:40:00 +0100

    window added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
-#include <QCoreApplication>
+#include "openglwindow.h"
+#include "gamewindow.h"
+#include <QtGui/QGuiApplication>
+
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    QGuiApplication a(argc, argv);
+    QSurfaceFormat format;
+    format.setSamples(16);
+
+    GameWindow window;
+    window.setFormat(format);
+
+    window.resize(800, 800);
+    window.show();
+
+    window.setAnimating(false);
 
     return a.exec();
 }
